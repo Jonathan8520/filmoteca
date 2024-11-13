47bdab2baded5c688461dd4eceeb210f539db8cd
Jonathan Levy <jonathan.levy2@etu.univ-paris1.fr>
2024-11-13 14:32:52 +0100

    router

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Initialiser Twig
+
 require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
@@ -15,18 +15,7 @@ try {
 }
 
 
-/////////////////////////////////////////////////////////// Fin pour la connexion
+require_once 'src/Router.php';
 
-
-// Récupérer les films depuis la base de données
-$films = $pdo->query("SELECT * FROM movie")->fetchAll(PDO::FETCH_ASSOC);
-
-$requestUri = $_SERVER['REQUEST_URI'];
-
-if ($requestUri === '/films') {
-    echo $twig->render('films.html.twig', ['films' => $films]);
-} elseif ($requestUri === '/contact') {
-    echo $twig->render('contact.html.twig');
-} else {
-    echo $twig->render('index.html.twig');
-}
+$router = new Router($twig, $pdo);
+$router->route();
