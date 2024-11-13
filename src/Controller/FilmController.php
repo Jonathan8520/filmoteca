9b83c59387ab0422371929607b2deb35f8ba3301
Jonathan Levy <jonathan.levy2@etu.univ-paris1.fr>
2024-11-13 16:27:47 +0100

    action Delete CRUD

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -17,7 +17,26 @@ class FilmController
 
     public function index()
     {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['action']) && $_GET['action'] === 'delete' && isset($_GET['id'])) {
+            $this->delete($_GET['id']);
+        }
+    
         $films = $this->pdo->query("SELECT * FROM movie")->fetchAll(PDO::FETCH_ASSOC);
         echo $this->twig->render('films.html.twig', ['films' => $films]);
     }
+
+    public function create() {}
+    public function read($id) {}
+    public function update($id) {}
+
+    public function delete($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM movie WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+    
+        // Rediriger après suppression
+        header('Location: /films');
+        exit();
+    }
+    
 }
